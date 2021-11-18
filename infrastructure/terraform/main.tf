f61b06157ad38d77c42cf5fc92589c10e4cf0b50
Leonardo-Wolo <Leonardo_Wolo@outlook.com>
2021-11-18 21:33:27 +0100

    Testing

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_public_ip" "iac-pIP" {
   resource_group_name = azurerm_resource_group.IAC_Production.name
   location            = azurerm_resource_group.IAC_Production.location
   allocation_method   = "Dynamic"
-  domain_name_label   = "team3-iac"
+  domain_name_label   = "iac-team3"
 }
 
 # Configure Microsoft Azure network interface
